e27eb59ec51c1da0e987e870f1d78b7fc4034520
nyarla <nyarladev@gmail.com>
2021-08-22 17:54:24 +0300

    More flexible Dash editing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,11 +12,14 @@ public class PlayerMovement : Transformer
     [SerializeField] private float _acceleration;
     [SerializeField] private float _dashDuration;
     [SerializeField] private float _dashDistance;
-    [SerializeField] [Range(0, 1)] private float _dashAcceleration;
+    [SerializeField] [Range(0, 1)] private float _dashStartAlign;
+    [SerializeField] [Range(0, 1)] private float _dashEndAlign;
     [SerializeField] private float _dashCooldown;
 
     private Vector2 _movementAxes;
     private bool _dashReady = true;
+
+    private int _freezeControls;
 
     private void Awake()
     {
@@ -26,7 +29,8 @@ public class PlayerMovement : Transformer
 
     private void FixedUpdate()
     {
-        Move();
+        if (_freezeControls == 0)
+            Move();
     }
 
     private void Move()
@@ -42,17 +46,19 @@ public class PlayerMovement : Transformer
         if (!_dashReady || dashDirection.magnitude <= 0)
             yield break;
         _dashReady = false;
+        _freezeControls++;
         BezierCurve curve = new BezierCurve(new Vector3[4]);
         Vector2 targetPoint = (Vector2) transform.position + dashDirection * _dashDistance;
         curve.SetPoint(0, transform.position);
-        curve.SetPoint(1, Vector2.Lerp(transform.position, targetPoint, _dashAcceleration));
-        curve.SetPoint(2, Vector2.Lerp(transform.position, targetPoint, 1 - _dashAcceleration));
+        curve.SetPoint(1, Vector2.Lerp(transform.position, targetPoint, _dashStartAlign));
+        curve.SetPoint(2, Vector2.Lerp(transform.position, targetPoint, _dashEndAlign));
         curve.SetPoint(3, targetPoint);
-        for (float i = 0; i < _dashDuration; i += Time.deltaTime)
+        for (float i = 0; i < _dashDuration * 1.3f; i += Time.deltaTime)
         {
            transform.position = curve.Evaluate(i / _dashDuration);
             yield return null;
         }
+        _freezeControls--;
         yield return new WaitForSeconds(_dashCooldown);
         _dashReady = true;
     }
